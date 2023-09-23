537a8d307f7735bbb9be1f2f6b0361c07c283e9c
kuri <kuri@172.30.1.13>
2023-09-23 20:40:48 +0900

    docs ...

--- src/kuri/calendar/Calendar.java
+++ src/kuri/calendar/Calendar.java
@@ -3,7 +3,7 @@ package kuri.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello kuri");
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
